446cdc9c29d8ec59964edfd5127251f65a6028fa
joaoAMG <joaofelipegqs@gmail.com>
2026-05-28 23:14:19 -0300

    Adicionando a model das classes Vendedor e notaFiscal no sistema

---
+++ projeto/src/models/NotaFiscal.ts
@@ -0,0 +1,22 @@
+export class NotaFiscal{
+    private static contadorNotaFiscal= 1;
+
+    id_nota: number;
+    numero_nota: string;
+    data_emissao: Date
+    valor_total: number;
+    id_cliente: number;
+    id_vendedor: number;
+    id_carro: number;
+
+    constructor(numero_nota: string, data_emissao: Date, valor_total: number, id_cliente: number, id_vendedor: number, id_carro: number){
+
+        this.id_nota = NotaFiscal.contadorNotaFiscal++
+        this.numero_nota = numero_nota
+        this.data_emissao = data_emissao
+        this.valor_total = valor_total
+        this.id_cliente = id_cliente
+        this.id_vendedor = id_vendedor
+        this.id_carro = id_carro
+    }
+}
