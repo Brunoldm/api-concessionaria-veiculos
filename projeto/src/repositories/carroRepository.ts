8749a60da260d1b9d7d6d75424ea064381a7736f
Brunoldm <bruno.ldm@hotmail.com>
2026-06-28 18:20:45 -0300

    Atualização do carroRepository mudando para MySql

--- projeto/src/repositories/carroRepository.ts
+++ projeto/src/repositories/carroRepository.ts
@@ -1,55 +1,143 @@
+import { executarComandoSQL } from "../database/mysql";
 import { Carro } from "../models/Carro";
 
 export class CarroRepository{
-    private static instance: CarroRepository;
-    private carroList : Carro [] = [];
+    private static instance: CarroRepository
 
-    private constructor() {}
+    static getCreateTableQuery(): string {
+        return `
+        CREATE TABLE IF NOT EXISTS Carro (
+            id_carro INT AUTO_INCREMENT PRIMARY KEY, 
+            marca VARCHAR(255) NOT NULL, 
+            modelo VARCHAR(255) NOT NULL,
+            ano INT NOT NULL,
+            placa VARCHAR(255) NOT NULL UNIQUE,
+            preco DECIMAL(10,2) NOT NULL,
+            cor VARCHAR(255) NOT NULL
+        );
+    `;
+    }  
 
-    public static getInstance (): CarroRepository{
-        if(! this.instance){
-            this.instance = new CarroRepository();
+    private constructor() { }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new CarroRepository()
         }
-        return this.instance;
+        return this.instance
     }
 
-    listaTodosCarros(): Carro[]{
-        return this.carroList;
+    async listaTodosCarros(): Promise<Carro[]> {
+        const linhas = await executarComandoSQL("SELECT id_carro, marca, modelo, ano, placa, preco, cor FROM Carro", []);
+
+        const carros: Carro[] = linhas.map((linha: any) => {
+            return new Carro(
+            linha.id_carro,
+            linha.marca,
+            linha.modelo,
+            Number(linha.ano),
+            linha.placa,
+            Number(linha.preco),
+            linha.cor);
+        });
+ 
+        return carros;        
     }
 
-    filtrarCarroPorID(id_carro: number): Carro | undefined{
-        return this.carroList.find( carro => carro.id_carro === id_carro);
+    async filtrarCarroPorID(id_carro: number): Promise<Carro | null>{
+        const linhas = await executarComandoSQL(
+            "SELECT id_carro, marca, modelo, ano, placa, preco, cor FROM Carro WHERE id_carro = ?", [id_carro]
+        );
+
+        if(linhas.length === 0){
+            return null;
+        }
+
+        const linha = linhas[0];
+        return new Carro(
+            linha.id_carro,
+            linha.marca,
+            linha.modelo,
+            Number(linha.ano),
+            linha.placa,
+            Number(linha.preco),
+            linha.cor);
     }
 
-    cadastrarCarro( carro : Carro){
-        this.carroList.push(carro);
+    async cadastrarCarro( carro : Carro): Promise<Carro>{
+        const resultado = await executarComandoSQL(
+            "INSERT INTO Carro (marca, modelo, ano, placa, preco, cor) VALUES (?, ?, ?, ?, ?, ?)",
+            [carro.marca, carro.modelo, carro.ano, carro.placa, carro.preco, carro.cor]
+        );
+
+        const idGerado = resultado.insertId;
+
+        const newCarro = new Carro(idGerado, carro.marca, carro.modelo, carro.ano, carro.placa, carro.preco, carro.cor);
+
+        console.log('Carro inserido com sucesso:', newCarro);
+        return newCarro;
     }
 
-    atualizarCarroPorID(id_carro: number, carroData: Carro): Carro {
+    async atualizarCarroPorID(id_carro: number, carroData: Carro): Promise<Carro> {
+        const query = `UPDATE Carro SET marca = ?, modelo = ?, ano = ?, placa = ?, preco = ?, cor = ? WHERE id_carro = ?; `;
 
-        const carro = this.carroList.find(carro => carro.id_carro === id_carro)!;
+        try {
+            const resultado = await executarComandoSQL(query, [
+                carroData.marca,
+                carroData.modelo,
+                carroData.ano,
+                carroData.placa,
+                carroData.preco,
+                carroData.cor,
+                id_carro
+            ]);
 
-        carro.marca = carroData.marca;
-        carro.modelo = carroData.modelo;
-        carro.ano = carroData.ano;
-        carro.placa = carroData.placa;
-        carro.preco = carroData.preco;
-        carro.cor = carroData.cor;
+        console.log("Carro atualizado com sucesso:", resultado);
+        return new Promise<Carro>((resolve)=>{
+                resolve(carroData);
+            })
 
-        return carro;
+    } catch (err: any) {
+        console.error(`Erro ao atualizar o carro ${id_carro}: ${err}`);
+        throw err;
+    }
+}
+
+    async apagarCarroPorID(carro: Carro): Promise<Carro> {
+    const query = "DELETE FROM Carro WHERE id_carro = ?;";
+
+    try {
+        await executarComandoSQL(query, [carro.id_carro]);
+        console.log("Carro removido com sucesso:", carro);
+
+        return new Promise<Carro>((resolve) => {
+            resolve(carro);
+        });
+
+    } catch (err: any) {
+        console.error(`Erro ao remover o carro ${carro.id_carro}: ${err}`);
+        throw err;
+    }
+}
+
+    async filtrarCarroPorPlaca(placa: string): Promise<Carro | null> {
+    const query = `SELECT id_carro, marca, modelo, ano, placa, preco, cor FROM Carro WHERE placa = ?; `;
+    const linhas = await executarComandoSQL(query, [placa]);
+
+    if (linhas.length === 0) {
+        return null;
     }
 
-    apagarCarroPorID(id_carro: number): Carro {
-        const indice = this.carroList.findIndex(carro => carro.id_carro === id_carro);
+    const linha = linhas[0];
 
-        const carroApagado = this.carroList[indice];
-
-        this.carroList.splice(indice,1);
-
-        return carroApagado;
-    }
-
-    filtrarCarroPorPlaca(placa: string): Carro | undefined{
-        return this.carroList.find(carro => carro.placa === placa);
-    }
+    return new Carro(
+        linha.id_carro,
+        linha.marca,
+        linha.modelo,
+        Number(linha.ano),
+        linha.placa,
+        Number(linha.preco),
+        linha.cor
+    );
+}
 }
