e4b69afd49b223137007d9a59e8f5b699423fe4b
Brunoldm <bruno.ldm@hotmail.com>
2026-06-28 21:30:38 -0300

    Adição das rotas do Carro e do Estoque

---
+++ projeto/src/router/router.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from 'express';
+import { EstoqueController } from "../controllers/estoqueController";
+import { CarroController } from "../controllers/carroController";
+
+const router = Router();
+
+const carroController = new CarroController();
+const estoqueController = new EstoqueController();
+
+// Rotas Carros
+router.get("/carros", (req: Request, res: Response) => {carroController.listarTodosCarros(req, res);});
+
+router.get("/carros/disponiveis", (req: Request, res: Response) => {carroController.obterCarrosDisponiveis(req, res);});
+
+router.get("/carros/:id", (req: Request, res: Response) => {carroController.filtrarCarroPorID(req, res);});
+
+router.post("/carros", (req: Request, res: Response) => {carroController.cadastrarNovoCarro(req, res);});
+
+router.put("/carros/:id", (req: Request, res: Response) => {carroController.atualizarCarroPorID(req, res);});
+
+router.delete("/carros/:id", (req: Request, res: Response) => {carroController.apagarCarroPorID(req, res);});
+
+// Rotas Estoques
+router.get("/estoques", (req: Request, res: Response) => {estoqueController.listaTodosRegistroEstoque(req, res);});
+
+router.get("/estoques/carro/:id_carro", (req: Request, res: Response) => {estoqueController.buscarEstoqueEspecificoDeCarro(req, res);});
+
+router.get("/estoques/:id", (req: Request, res: Response) => {estoqueController.buscarRegistroDeEstoque(req, res);});
+
+router.post("/estoques", (req: Request, res: Response) => {estoqueController.criarNovoRegistroEstoque(req, res);});
+
+router.put("/estoques/:id", (req: Request, res: Response) => {estoqueController.atualizarEstoque(req, res);});
+
+router.delete("/estoques/:id", (req: Request, res: Response) => {estoqueController.removerRegistroEstoque(req, res);});
+
+export default router;
